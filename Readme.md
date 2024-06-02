8e923e3cf4c9bde3e06c8df0d14ecc0be2335780
Fadouse <fadouse@163.com>
2024-06-02 19:42:47 +0800

    finished

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Just a simple Example for Java File's Hash Check.
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,79 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args){
+        String inputFilePath;
+
+        if(Objects.equals(args[0], "0")){
+            inputFilePath = getJarPath();
+        }else{
+            inputFilePath = args[0];
+        }
+
+        if(!(new File(inputFilePath)).exists())
+            System.out.println("File isn't exists, Pleas input correct file path");
+
+        try {
+            byte[] sha256 = calculateSHA256(inputFilePath);
+            System.out.println("SHA256: " +  bytesToHex(sha256));
+        }catch (IOException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static String getJarPath() {
+        String path = Main.class.getProtectionDomain().getCodeSource()
+                .getLocation().getFile();
+        try {
+            path = URLDecoder.decode(path, "UTF-8");
+        } catch (UnsupportedEncodingException ignored) {}
+
+        File jarFile = new File(path);
+
+        File parent = jarFile.getParentFile();
+        return parent.getAbsolutePath()+"\\"+jarFile.getName();
+    }
+
+
+    public static byte[] calculateSHA256(String filePath) throws IOException, NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        try (
+                FileInputStream fis = new FileInputStream(filePath);
+                FileChannel channel = fis.getChannel();
+                DigestInputStream ignored = new DigestInputStream(fis, digest)) {
+
+            ByteBuffer buffer = ByteBuffer.allocate(8192); // 8 KB buffer
+            while (channel.read(buffer) != -1) {
+                buffer.flip();
+                digest.update(buffer);
+                buffer.clear();
+            }
+
+            return digest.digest();
         }
     }
+
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte b : bytes) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
+    }
+
+
 }
